b92e5853939504e7358b3290733ca31b821d1a1f
SrzStar <siriphopsukserm@gmail.com>
2026-08-19 19:59:39 +0700

    Create FantasyClass-Buffer.lua

---
+++ FantasyClass-Buffer.lua
@@ -0,0 +1,34 @@
+local ClassModule = {}
+
+-- ฟังก์ชันช่วยสร้าง Buffer ลดความซ้ำซ้อนของโค้ด
+local function createSkillBuffers(skillName)
+    local baseBuffer = buffer.fromstring(string.char(31, 0, #skillName, 0) .. skillName)
+    local buffers = { baseBuffer }
+    for i = 1, 6 do
+        local tierSkillName = skillName .. "-" .. i
+        local packetString = string.char(31, 0, #tierSkillName, 0) .. tierSkillName
+        table.insert(buffers, buffer.fromstring(packetString))
+    end
+    return buffers
+end
+
+-- สร้าง Buffer แต่ละสกิล
+local mageExplosion     = createSkillBuffers("ManaExplosion")
+local bardInsult        = createSkillBuffers("Insult")
+local bardInspiration   = createSkillBuffers("Inspiration")
+local pyroBurst         = createSkillBuffers("FireBurst")
+local wizardBolt        = createSkillBuffers("LightningBolt")
+local wizardStorm       = createSkillBuffers("LightningStorm")
+local druidWisp         = createSkillBuffers("WispSummon")
+local druidAura         = createSkillBuffers("EntanglingAura")
+
+-- จัดหมวดหมู่ตาม Class
+ClassModule.Classes = {
+    ["Mage"]       = { mageExplosion },
+    ["Bard"]       = { bardInsult, bardInspiration },
+    ["Pyromancer"] = { pyroBurst },
+    ["Wizard"]     = { wizardBolt, wizardStorm },
+    ["Druid"]      = { druidWisp, druidAura }
+}
+
+return ClassModule
